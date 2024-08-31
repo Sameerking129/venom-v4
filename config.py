433dbe6f0693cf495e5f420926696914c6c6956a
Sameerking129 <ssd458515@gmail.com>
2024-08-31 23:44:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7477569578:AAFm6p06WeYizfhXX7z2XocRcHejeCNLiVg")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Syedsameer09")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6348337956")
 
 
 
